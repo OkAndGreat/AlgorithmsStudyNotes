941d15180c729bce0dbd99b392772aa67f888566
wzt <2238091763@qq.com>
2021-04-24 10:55:38 +0800

    [updata]日常算法刷题学习

--- src/test.java
+++ src/test.java
@@ -3,54 +3,30 @@ import java.util.*;
 public class test {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        TreeNode root = new TreeNode(3);
-        root.left = new TreeNode(9);
+        String s="123";
+        System.out.println(s.substring(0,1));
+        StringBuilder stringBuilder = new StringBuilder();
     }
 }
 
 class Solution {
-    List<List<Integer>> result;
-    public List<List<Integer>> pathSum(TreeNode root, int target) {
-        result=new ArrayList();
-        List<Integer> toAddList=new ArrayList();
-        if(root==null) return result;
-        recur(root,target,0,toAddList);
-        return result;
-    }
-    void recur(TreeNode root, int target,int num,List<Integer> toAddList){
-        if(root.val+num==target){
-            result.add(toAddList);
-        }else if(root.val+num>target){
-            return;
-        }else{
-            if(root.left!=null) {
-                toAddList.add(root.left.val);
-                recur(root.left,target,num,toAddList);
+    HashMap<Character,Boolean> hashMap=new HashMap();
+    public char firstUniqChar(String s) {
+        char c;
+        Boolean flag=true;
+        if(s.length()==0) return ' ';
+        for(int i=0;i<s.length();i++){
+            c=s.charAt(i);
+            if(hashMap.containsKey(c)) continue;
+            hashMap.put(c,true);
+            for(int j=i+1;i<s.length();j++){
+                if(s.charAt(j)==c){
+                    flag=false;
+                    break;
+                }
             }
-            if(root.right!=null) {
-                toAddList.add(root.right.val);
-                recur(root.right,target,num,toAddList);
-            }
+            if(flag) return c;
         }
-    }
-}
-
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        return ' ';
     }
 }

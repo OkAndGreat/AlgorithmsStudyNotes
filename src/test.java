3ba21531febb0e0fc2ac4c9a0dbcb662a50dc036
wzt <2238091763@qq.com>
2021-05-16 09:55:06 +0800

    [updata]日常算法学习刷题

--- src/test.java
+++ src/test.java
@@ -3,30 +3,22 @@ import java.util.*;
 public class test {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String s="123";
-        System.out.println(s.substring(0,1));
-        StringBuilder stringBuilder = new StringBuilder();
+
+        solution.reverseLeftWords("1",2);
     }
 }
 
 class Solution {
-    HashMap<Character,Boolean> hashMap=new HashMap();
-    public char firstUniqChar(String s) {
-        char c;
-        Boolean flag=true;
-        if(s.length()==0) return ' ';
-        for(int i=0;i<s.length();i++){
-            c=s.charAt(i);
-            if(hashMap.containsKey(c)) continue;
-            hashMap.put(c,true);
-            for(int j=i+1;i<s.length();j++){
-                if(s.charAt(j)==c){
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag) return c;
-        }
-        return ' ';
+    public String reverseLeftWords(String s, int n) {
+//        StringBuilder sb=new StringBuilder();
+//        sb.append(s.substring(n-1,s.length()-1));
+//        sb.append(s.substring(0,n-1));
+
+        System.out.println(getManIn3(1,3,2));
+        return "sb.toString()";
+    }
+
+    int getManIn3(int a,int b,int c){
+        return a>b?(Math.max(a, c)):(Math.max(b, c));
     }
 }

b60f9fa42f75975129ace219c55b7aa5654bb32f
wzt <2238091763@qq.com>
2021-06-06 21:15:42 +0800

    [updata]日常算法学习刷题

--- src/test.java
+++ src/test.java
@@ -3,33 +3,45 @@ import java.util.*;
 public class test {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {1, 2};
-        System.out.println(Arrays.deepToString(solution.permute(nums).toArray()));
+        int[] nums = {2, 2, 1, 1};
+        System.out.println(Arrays.deepToString(solution.permuteUnique(nums).toArray()));
     }
 
+}
 
-    static class Solution {
-        List<List<Integer>> res;
-        List<Integer> temp;
+class Solution {
+    List<List<Integer>> res;
+    //0说明还未添加 1则相反
+    int[] vis;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        res=new ArrayList();
+        vis=new int[nums.length];
+        Arrays.sort(nums);
+        dfs(0,nums,new ArrayList());
+        return res;
+    }
 
-        public List<List<Integer>> permute(int[] nums) {
-            res = new ArrayList();
-            temp = new ArrayList();
-            dfs(0, temp, nums);
-            return res;
+    void dfs(int i,int[] nums,List<Integer> temp){
+        if(i==nums.length){
+            res.add(new ArrayList(temp));
+            return;
         }
-
-        void dfs(int i, List<Integer> temp, int[] nums) {
-            if (i == nums.length) {
-                res.add(new ArrayList<>(temp));
-                return;
-            }
-            for (int j = 0; j < nums.length; j++)
-                if (!temp.contains(nums[j])) {
-                    temp.add(nums[j]);
-                    dfs(i + 1, temp, nums);
-                    temp.remove(temp.indexOf(nums[j]));
-                }
+        for(int j=0;j<nums.length;j++){
+            //考虑重复序列
+            if(vis[j]==1||(j>0&&nums[j]==nums[j-1]&&vis[j-1]==0)) continue;
+            temp.add(nums[j]);
+            vis[j]=1;
+            dfs(i+1,nums,temp);
+            //体现回溯
+            vis[j]=0;
+            temp.remove(i);
+            //temp.remove(temp.indexOf(nums[j]));
+            // 是错误的
+            //因为存在多个相同元素，可能得到错误的下标
         }
     }
 }
+
+
+
+

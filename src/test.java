1e417bc382a8441c38b2399d3b191943168eea14
wzt <2238091763@qq.com>
2021-06-13 19:00:36 +0800

    [updata]日常算法学习刷题

--- src/test.java
+++ src/test.java
@@ -3,34 +3,43 @@ import java.util.*;
 public class test {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String[] strs=new String[]{"1"};
-
+        String[] strs = new String[]{"1"};
+        solution.combine(4, 2);
     }
 
 }
 
 class Solution {
-    List<String> res = new ArrayList();
-    StringBuilder sb = new StringBuilder();
-    String[] strs = new String[]{"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    public List<String> letterCombinations(String digits) {
-        dfs(digits,0);
+    List<List<Integer>> res = new ArrayList();
+    int n;
+    int k;
+
+    public List<List<Integer>> combine(int n, int k) {
+        this.n = n;
+        this.k = k;
+        for (int i = 1; i <= n - k + 1; i++)
+            fun(i, new ArrayList());
         return res;
     }
 
-    void dfs(String str,int idx){
-        if(idx==str.length()){
-            res.add(sb.toString());
+    void fun(int idx, List<Integer> temp) {
+        temp.add(idx);
+        dfs(idx + 1, temp);
+    }
+
+    void dfs(int idx, List<Integer> temp) {
+        if (temp.size() == k) {
+            res.add(new ArrayList(temp));
             return;
         }
-        for(int i = 0; i<strs[Integer.parseInt(String.valueOf(str.charAt(idx)))-2].length(); i++){
-            sb.append(strs[Integer.parseInt(String.valueOf(str.charAt(idx)))-2].charAt(i));
-            dfs(str,idx+1);
-            sb.deleteCharAt(sb.toString().length()-1);
+        for (int i = idx; i <= n; i++) {
+            dfs(i + 1, temp);
+            temp.add(i);
+            dfs(i + 1, temp);
+            temp.remove(temp.indexOf(i));
         }
     }
 }
-
 
 
 

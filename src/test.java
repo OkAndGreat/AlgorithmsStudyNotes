b94d92a96bb0a866930a0ac9249fa5cda438f6a8
wzt <2238091763@qq.com>
2021-06-17 09:56:00 +0800

    [updata]日常算法学习刷题

--- src/test.java
+++ src/test.java
@@ -4,33 +4,40 @@ public class test {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] a=new int[]{2,3,6,7};
-        solution.combinationSum(a,7);
     }
 
 }
 
 class Solution {
-    List<List<Integer>> res = new ArrayList();
+    int[] vis;
     int target;
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    List<List<Integer>> res = new ArrayList();
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         this.target=target;
-        for(int i=0;i<candidates.length;i++)
-            dfs(candidates,i,0,new ArrayList<Integer>());
+        Arrays.sort(candidates);
+        vis=new int[candidates.length];
+        List<Integer> temp = new ArrayList();
+        for(int i=0;i<candidates.length;i++){
+            temp.add(candidates[i]);
+            dfs(i+1,temp,candidates[i],candidates);
+            temp.clear();
+        }
         return res;
     }
 
-    void dfs(int[] candidates,int idx,int value,List<Integer> temp){
+    void dfs(int idx,List<Integer> temp,int value,int[] candidates){
         if(value==target){
             res.add(new ArrayList(temp));
             return;
         }
         if(value>target) return;
         for(int i=idx;i<candidates.length;i++){
-            value+=candidates[i];
+            if(i>0&&candidates[i-1]==candidates[i]&&vis[i-1]==0) continue;
             temp.add(candidates[i]);
-            dfs(candidates,i+1,value,temp);
+            value+=candidates[i];
+            dfs(i+1,temp,value,candidates);
             value-=candidates[i];
-            temp.remove(temp.indexOf(candidates[i]));
+            temp.remove(temp.size()-1);
         }
     }
 }

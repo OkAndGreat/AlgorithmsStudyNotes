a2a38ec7fec83b2d26c2b917136a3a787454d51a
wzt <2238091763@qq.com>
2021-07-28 09:24:59 +0800

    [updata]日常算法学习刷题

--- src/test.java
+++ src/test.java
@@ -1,41 +1,78 @@
 import java.util.*;
 
+//[9]
+//[1,9,9,9,9,9,9,9,9,9]
 public class test {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] a=new int[]{2,3,6,7};
+        ListNode l1 = new ListNode(9);
+        ListNode l2 = new ListNode(9);
+        ListNode curL2 = l2;
+        curL2.next = new ListNode(9);
+        curL2 = curL2.next;
+        curL2.next = new ListNode(9);
+        curL2 = curL2.next;
+        curL2.next = new ListNode(9);
+        curL2 = curL2.next;
+        curL2.next = new ListNode(9);
+        curL2 = curL2.next;
+        curL2.next = new ListNode(9);
+        curL2 = curL2.next;
+        curL2.next = new ListNode(9);
+        curL2 = curL2.next;
+        curL2.next = new ListNode(9);
+        curL2 = curL2.next;
+        curL2.next = new ListNode(9);
+        curL2 = curL2.next;
+        curL2.next = new ListNode(1);
+
+
     }
 
 }
 
-
 class Solution {
-    List<List<Integer>> res = new ArrayList();
-    public List<List<Integer>> subsets(int[] nums) {
-        Stack stack;
-        res.add(null);
-        dfs(nums);
+    ListNode node;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int val = getNodeVal(l1) + getNodeVal(l2);
+        node = new ListNode(val % 10);
+        val /= 10;
+        ListNode curNode = node;
+        while (val != 0) {
+            int num = val % 10;
+            curNode.next = new ListNode(num);
+            curNode = curNode.next;
+            val /= 10;
+        }
+        return node;
+    }
+
+    int getNodeVal(ListNode node) {
+        int res = 0;
+        int a = 1;
+        do {
+            res += node.val * a;
+            a *= 10;
+            node = node.next;
+        } while (node != null);
         return res;
     }
+}
 
-    void dfs(int[] nums){
-        List<Integer> temp = new ArrayList();
-        for(int i=0;i<nums.length;i++){
-            temp.add(nums[i]);
-            dfs(temp,nums,i);
-            temp.remove(temp.size()-1);
-        }
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
     }
 
-    void dfs(List<Integer> temp,int[] nums,int idx){
-        res.add(new ArrayList(temp));
-        if(idx==nums.length) return;
-        for(int i=idx;i<nums.length;i++){
-            dfs(temp,nums,i+1);
-            temp.add(nums[i]);
-            dfs(temp,nums,i+1);
-            temp.remove(temp.size()-1);
-        }
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
 
